092dd4f46347043bed6b223161b27de05a030908
Jonas Harriehausen <jonas.harriehausen@uni-giessen.de>
2026-01-28 10:19:08 +0100

    proof of concept reimplementation

--- reimplementation.py
+++ reimplementation.py
@@ -1,14 +1,13 @@
 # %%
-import matplotlib.pyplot as plt
 from sksurv.datasets import load_veterans_lung_cancer
 from sksurv.ensemble import RandomSurvivalForest
 from sksurv.preprocessing import OneHotEncoder
 import pandas as pd
 import numpy as np
-from random import shuffle
+from random import shuffle, sample
+from typing import List
 
 # %%
-
 
 class LocalSurvivalRF(RandomSurvivalForest):
     def __init__(
@@ -49,8 +48,44 @@ class LocalSurvivalRF(RandomSurvivalForest):
             max_samples=max_samples,
             low_memory=low_memory,
         )
-        self.local_features = local_features
+        self.local_features = set(local_features)
         self.all_features = all_features
+
+
+class FederatedSurvivalRF(RandomSurvivalForest):
+    def __init__(self, local_models: List[LocalSurvivalRF], **kwargs):
+        super().__init__(**kwargs)
+        self.local_models = local_models
+        self.features = local_models[0].all_features
+        self.feature_names_in_ = local_models[0].feature_names_in_
+        self.estimators_ = []
+        for model in local_models:
+            self.estimators_.extend(model.estimators_)
+        self.n_estimators = len(self.estimators_)
+
+
+        self.estimator_features = []
+        for estimator in self.estimators_:
+            tree_features = estimator.tree_.feature
+            tree_features_names = set([self.features[i] for i in tree_features if i >= 0])
+            self.estimator_features.append(tree_features_names)
+
+    def fit(self, *args, **kwargs):
+        raise NotImplementedError("Federated model cannot be fit directly.")
+
+    def update_local_models(self, local_size: int = None):
+        for model in self.local_models:
+            valid_estimators = []
+            for estimator, feat_set in zip(self.estimators_, self.estimator_features):
+                # find all estimators that only use features available locally
+                if feat_set.issubset(model.local_features):
+                    valid_estimators.append(estimator)
+            
+            if local_size is None:
+                model.estimators_ = valid_estimators
+            else:
+                model.estimators_ = sample(valid_estimators, local_size)
+            model.n_estimators = len(model.estimators_)
 
 
 def federate_data(
@@ -71,34 +106,47 @@ def federate_data(
         cols_to_drop = X_cur.columns.to_series().sample(
             frac=drop_cols_precentage, random_state=42 + i
         )
-        # X_cur = X_cur.drop(columns=cols_to_drop)
+
+        # this implementation does not work with fully missing columns
+        # X_cur = X_cur.drop(columns=cols_to_drop) 
+
+
+        # nan-columns for missing features in federated setting
+        # these have to exist for the trees to work in this framework
         X_cur.loc[:, cols_to_drop] = np.nan
         X_splits.append(X_cur)
         y_splits.append(Y[start:end])
     return X_splits, y_splits
 
 
-X, y = load_veterans_lung_cancer()
+X_base, Y_base = load_veterans_lung_cancer()
 client_count = 3
-X_splits, Y_splits = federate_data(X, y, client_count)
+X_splits, Y_splits = federate_data(X_base, Y_base, client_count)
 
-models = []
 Xt_splits = [OneHotEncoder().fit_transform(x_split) for x_split in X_splits]
 
-all_features = OneHotEncoder().fit_transform(X).columns.to_list()
+all_features = OneHotEncoder().fit_transform(X_base).columns.to_list()
 
-
+models = []
 for i in range(client_count):
     X, Y = Xt_splits[i], Y_splits[i]
 
     local_features = X.columns[~X.isna().all()].to_list()
-
     model = LocalSurvivalRF(local_features, all_features).fit(X, Y)
-
     models.append(model)
-
-models
+    print(f'local prediction of client {i} with {len(model.estimators_)} estimators:')
+    print(model.predict(X.iloc[:10]))
 
 # %%
 
-model.estimators_
+federated_model = FederatedSurvivalRF(models, n_estimators=300)
+federated_model.update_local_models()
+# example prediction using federated model
+
+print(f'federated prediction with {len(federated_model.estimators_)} estimators on data from client 1:')
+print(federated_model.predict(Xt_splits[0].iloc[:10]))
+
+# preditctions on updated local models
+for i, (model, data) in enumerate(zip(models, Xt_splits)):
+    print(f'local prediction of client {i} with {len(model.estimators_)} estimators:')
+    print(model.predict(data.iloc[:10]))
